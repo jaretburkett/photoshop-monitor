62351e4615fbbf9e00d4e88cfb4e5f33ba7b6ca9
Jaret Burkett <jaretburkett@gmail.com>
2021-08-30 16:15:16 -0600

    Wrap program in a try catch to prevent any possible errors from crashing it.

--- main.js
+++ main.js
@@ -28,55 +28,57 @@ const delay = (ms) => {
 
 
 async function main() {
-  const errorWindow = photoshop.getErrorWindow();
+  try {
+    const errorWindow = photoshop.getErrorWindow();
 
-  // get a screenshot of active window. 
-  if (errorWindow) {
-    // bring error window to front
-    errorWindow.bringToTop();
-    // give a second to process that
-    await delay(1000);
-
-    const { x, y, width, height } = errorWindow.getBounds();
-    const img = robot.screen.capture(x, y, width, height);
-    // const img = robot.screen.capture();
-    await imageTools.saveImage(img, tempFilePath);
-    const ocrText = await imageTools.findText(tempFilePath);
-
-    // append to log
-    let logTxt = '\r\n*********** NEW ****************\r\n';
-    logTxt += ocrText;
-    writeToLog(logTxt, 'OCR_log');
-    // run through all phrase actions to see if one matches
-    let PA = null;
-    for (let i = 0; i < phraseActions.length; i++) {
-      for (let p = 0; p < phraseActions[i].phrases.length; p++) {
-        const phrase = phraseActions[i].phrases[p];
-        if (ocrText.toLowerCase().includes(phrase.toLowerCase())) {
-          // we have a match. Put it in the actions
-          PA = phraseActions[i];
+    // get a screenshot of active window. 
+    if (errorWindow) {
+      // bring error window to front
+      errorWindow.bringToTop();
+      // give a second to process that
+      await delay(1000);
+  
+      const { x, y, width, height } = errorWindow.getBounds();
+      const img = robot.screen.capture(x, y, width, height);
+      // const img = robot.screen.capture();
+      await imageTools.saveImage(img, tempFilePath);
+      const ocrText = await imageTools.findText(tempFilePath);
+  
+      // append to log
+      let logTxt = '\r\n*********** NEW ****************\r\n';
+      logTxt += ocrText;
+      writeToLog(logTxt, 'OCR_log');
+      // run through all phrase actions to see if one matches
+      let PA = null;
+      for (let i = 0; i < phraseActions.length; i++) {
+        for (let p = 0; p < phraseActions[i].phrases.length; p++) {
+          const phrase = phraseActions[i].phrases[p];
+          if (ocrText.toLowerCase().includes(phrase.toLowerCase())) {
+            // we have a match. Put it in the actions
+            PA = phraseActions[i];
+          }
         }
       }
-    }
-
-    // see if we need to run actions
-    if (PA) {
-      console.log('Running Action: ' + PA.name);
-      try {
-        errorWindow.bringToTop();
-        await delay(500);
-        await runAction(PA.actions);
-      } catch (e) {
-        console.log(e);
+  
+      // see if we need to run actions
+      if (PA) {
+        console.log('Running Action: ' + PA.name);
+        try {
+          errorWindow.bringToTop();
+          await delay(500);
+          await runAction(PA.actions);
+        } catch (e) {
+          console.log(e);
+        }
       }
+  
+    } else {
+      // console.log('Photoshop not active window');
     }
-
-  } else {
-    // console.log('Photoshop not active window');
+  } catch (e) {
+    console.log('ERROR: ', e);
   }
-
   await delay(10000);
-
   main();
 }
 
