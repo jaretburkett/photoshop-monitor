f85970aa1122096959507f24a2e3340d75f1d0e1
Jaret Burkett <jaretburkett@gmail.com>
2021-08-30 16:03:45 -0600

    Added new phrase. Added rotating log for open windows to help with other errors.

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
-const activeWindow = require('active-win');
 const robot = require("robotjs");
 const fs = require('fs');
 const path = require('path');
 const imageTools = require('./lib/imageTools');
-const {runAction} = require('./lib/actions');
+const { runAction } = require('./lib/actions');
 const phraseActions = require('./lib/phrasesActions');
 const package = require('./package.json');
 const photoshop = require('./lib/photoshop');
+const { writeToLog } = require('./lib/logWriter');
 
 const tempFolder = path.join(__dirname, 'temp');
 const tempFilePath = path.join(tempFolder, 'activeWindow.png');
-const ocrLogPath = path.join(tempFolder, 'OCR_log.txt');
-
-
-const maxOCRLogLines = 500;
 
 // make the temp folder if it does not exist
 if (!fs.existsSync(tempFolder)) {
@@ -40,53 +36,39 @@ async function main() {
     errorWindow.bringToTop();
     // give a second to process that
     await delay(1000);
-    
+
     const { x, y, width, height } = errorWindow.getBounds();
     const img = robot.screen.capture(x, y, width, height);
     // const img = robot.screen.capture();
     await imageTools.saveImage(img, tempFilePath);
-    // console.log('Reading OCR for strings')
-    const start = new Date()
     const ocrText = await imageTools.findText(tempFilePath);
-    const stop = new Date()
-    // console.log('OCR Text:', ocrText );
-    // write text in ocr log
-    let logTxt = '';
-    if(fs.existsSync(ocrLogPath)){
-      logTxt = fs.readFileSync(ocrLogPath, 'utf8');
-    }
 
-    // append to log text
-    logTxt += '\r\n \r\n*********** NEW ****************\r\n';
+    // append to log
+    let logTxt = '\r\n*********** NEW ****************\r\n';
     logTxt += ocrText;
-
-    const logTxtArr = logTxt.split(/\r?\n/);
-    // remove lines until we have desired amount
-    while(true){
-      if(logTxtArr.length > maxOCRLogLines){
-        logTxtArr.shift();
-      } else {
-        break;
-      }
-    }
-    fs.writeFileSync(ocrLogPath, logTxtArr.join('\r\n'), 'utf8');
-    // console.log(`OCR took ${(stop - start)/1000} seconds`)
+    writeToLog(logTxt, 'OCR_log');
     // run through all phrase actions to see if one matches
     let PA = null;
-    for(let i = 0; i < phraseActions.length; i++){
-      for(let p = 0; p < phraseActions[i].phrases.length; p++){
+    for (let i = 0; i < phraseActions.length; i++) {
+      for (let p = 0; p < phraseActions[i].phrases.length; p++) {
         const phrase = phraseActions[i].phrases[p];
-        if(ocrText.toLowerCase().includes(phrase.toLowerCase())){
+        if (ocrText.toLowerCase().includes(phrase.toLowerCase())) {
           // we have a match. Put it in the actions
           PA = phraseActions[i];
         }
       }
     }
-    
+
     // see if we need to run actions
-    if(PA){
+    if (PA) {
       console.log('Running Action: ' + PA.name);
-      await runAction(PA.actions);
+      try {
+        errorWindow.bringToTop();
+        await delay(500);
+        await runAction(PA.actions);
+      } catch (e) {
+        console.log(e);
+      }
     }
 
   } else {
